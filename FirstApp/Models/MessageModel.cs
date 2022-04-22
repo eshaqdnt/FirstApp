aa8afba002f486b91042d06e87a446eacd0ca9b0
eshaqdnt <jabbarieshaq.1993@gmail.com>
2022-04-22 19:29:20 +0430

    Add Message model

---
+++ FirstApp/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApp.Models
+{
+    public class MessageModel
+    {
+        public string FullName { get; set; }
+        public string Subject { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+    }
+}
